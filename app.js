514ebc1c1cb11f9becdad79b7d76a709daad4227
Allie Pacyga <alliep@allies-air.attlocal.net>
2020-06-08 22:29:00 -0500

    tweaked more on mobile first size

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('jQuery is connected', $)
+
+
+$(document).ready(function() {
+    $('#about').click(function(event) {
+      $('body, html').animate({
+        scrollTop: $("#aboutContent").offset().top
+      }, 600);
+    });
+  });
+
+  $(document).ready(function() {
+    $('#projects').click(function(event) {
+      $('body, html').animate({
+        scrollTop: $("#projectsContent").offset().top
+      }, 600);
+    });
+  });
+
+
+  $(document).ready(function() {
+    $('#contact').click(function(event) {
+      $('body, html').animate({
+        scrollTop: $("#contactContent").offset().top
+      }, 600);
+    });
+  });
